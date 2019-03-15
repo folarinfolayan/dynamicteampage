52048fbe24fe503146cccb5a047eb624c04c4e4b
folarifolayan <f_folayan@fanshawe.ca>
2019-03-15 16:49:05 -0400

    git commit

---
+++ js/dynamicteam.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("Folarin");
+  if (x.innerHTML === "Hello, my name is Folarin Folayan. I am in charge of all creative duties. Throughout our intergrated project I lead the charge in all creative choices, while of course I took my partner's opinions and ideas into consideration. I am not as talent as my partner when it comes to coding but he is able to cover one of my weaknesses with his abilities. Anything pertianing to design, colours or graphic choices are usually handled by me. Originally, I am from Toronto, Ontario and I grew up there for my whole life, until I came to London for schooling in Interactive Media Developement.") {
+    x.innerHTML = "Ravi's information would be put here";
+  } else {
+    x.innerHTML = "Hello, my name is Folarin Folayan. I am in charge of all creative duties. Throughout our intergrated project I lead the charge in all creative choices, while of course I took my partner's opinions and ideas into consideration. I am not as talent as my partner when it comes to coding but he is able to cover one of my weaknesses with his abilities. Anything pertianing to design, colours or graphic choices are usually handled by me. Originally, I am from Toronto, Ontario and I grew up there for my whole life, until I came to London for schooling in Interactive Media Developement.";
+  }
+}
